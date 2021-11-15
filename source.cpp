3928ad0f641864cbf4814c7a75c454594c47460e
thinh <thinhbk00@gmail.com>
2021-11-15 14:38:16 +0700

    tich

--- source.cpp
+++ source.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int hieu(int a, int b);
-
+int tich(int a, int b);
 int main()
 {
 	int a, b;
@@ -11,11 +11,14 @@ int main()
 	cout << "Nhap so nguyen thu 2: ";
 	cin >> b;
 	hieu(a, b);
-
+	tich(a, b);
 	system("pause");
 	return 0;
 }
 
 int hieu(int a, int b) {
 	return a - b;
+}
+int tich(int a, int b) {
+	return a * b;
 }
